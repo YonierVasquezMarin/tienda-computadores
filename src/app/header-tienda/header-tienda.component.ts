5ef4d602f2e2bb578bbcfb7a94b7be282b019072
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-06-30 16:12:09 -0500

    Header agregado

---
+++ src/app/header-tienda/header-tienda.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header-tienda',
+  templateUrl: './header-tienda.component.html',
+  styleUrls: ['./header-tienda.component.scss']
+})
+export class HeaderTiendaComponent {
+
+}
